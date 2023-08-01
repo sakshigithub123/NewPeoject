6552e41e0229482c542bd6383510a5e4d035856e
Sajjan <sajjanraut1998@gmail.com>
2023-08-01 15:32:27 +0530

    added two sop

--- src/com/demo/Demo1.java
+++ src/com/demo/Demo1.java
@@ -4,5 +4,7 @@ public class Demo1
 	public static void main(String [] args)
 	{
 		System.out.println("Hello Git");
+		System.out.println(" how are you");
+		System.out.println(" i am fine");
 	}
 }
